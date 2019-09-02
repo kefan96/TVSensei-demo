945c67781a8d1f85d3a3b482c45e6968869a7f6f
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2019-09-01 20:33:37 -0400

    solgan interactive

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$('#slogan-en').on('click', () => {
+    $('#slogan-en').addClass('hide');
+    $('#slogan-jp').removeClass('hide');
+});
+
+$('#slogan-jp').on('click', () => {
+    $('#slogan-jp').addClass('hide');
+    $('#slogan-en').removeClass('hide');
+});
