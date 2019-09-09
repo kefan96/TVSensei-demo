ed847985f2bfd41c106ff13b10ac1696fe3d195c
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2019-09-09 15:04:19 -0400

    database branch init

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
@@ -20,6 +25,6 @@ app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
-app.listen(3000, () => {
-    console.log("TVSensei Listen on Port 3000");
+app.listen(PORT, () => {
+    console.log("TVSensei Listen on Port " + PORT);
   });
