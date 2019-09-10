2f76f1f9838125ec6defa14620107168b68a9111
Nikita Lilichenko <nikita.lilichenko@gmail.com>
2019-09-10 11:22:54 -0400

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,47 +1,62 @@
-// $('#password, #confirmpassword').on('keyup', function () {
-//     if ($('#password').val() == $('#confirmpassword').val()) {
-//       $('#message').html('Matching').css('color', 'green');
-//     } else 
-//       $('#message').html('Not Matching').css('color', 'red');
-//   })
-
-
-
-$("#signupform").submit(function(e) {
+$("#signupform").submit(function (e) {
     e.preventDefault();
 });
 
-var fname = document.getElementById("firstname")
+var inputfname = document.getElementById("firstname").value;
+localStorage.setItem("fname", inputfname);
+
 var password = document.getElementById("password");
 var confirmpassword = document.getElementById("confirmpassword");
 
-function submission() {
-    if (validatePSW()) {
-        if (password.value == confirmpassword.value) {
-            $("#loginModal").modal('show');
-            window.location.href = '/lesson';
-            
-            return true;
-      }
-        else {
-        document.getElementById("errormsg").innerHTML = "The two provided passwords do not match.";
-        password.value = "";
-        confirmpassword.value = "";
-        return false;
-    }
-    
-  }
-}
-
-function validatePSW() {
+function passReqs() {
     var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
     if (password.value.match(passw)) {
-       return true;
-    }
-    else {
-        document.getElementById("errormsg").innerHTML = "The provided password does not meet one or all of the requirements above.";
-        password.value = "";
-        confirmpassword.value = "";
-        return false;
+        return true;
     }
 }
+
+function submission() {
+    if (passReqs()) {
+        if (password.value == confirmpassword.value) {
+            document.getElementById("confirmpassword").style.border = "1px solid green";
+            sessionStorage.loadModal = 'true';
+            window.location.href = '/lesson';
+            return true;
+        }
+        else {
+            document.getElementById("errormsg").style.color = 'red';
+            document.getElementById("errormsg").innerHTML = "The two provided passwords do not match.";
+            document.getElementById("password").style.border = "1px solid red";
+            document.getElementById("confirmpassword").style.border = "1px solid red";
+            password.value = "";
+            confirmpassword.value = "";
+            return false;
+        }
+
+    }
+}
+
+function strongPassword() {
+    if (passReqs()) {
+        document.getElementById("errormsg").style.color = 'green';
+        document.getElementById("errormsg").innerHTML = "The provided password is strong!";
+        document.getElementById("password").style.border = "1px solid green";
+        document.getElementById("confirmpassword").style.border = '1px solid #ced4da';
+    }
+    else {
+        document.getElementById("errormsg").style.color = 'red';
+        document.getElementById("errormsg").innerHTML = "The provided password does not meet one or all of the requirements above.";
+        document.getElementById("password").style.border = "1px solid red";
+    }
+}
+
+function loadProfileModal() {
+    if (sessionStorage.loadModal == 'true') {
+        $('#profileModal').modal('show');
+    }
+    sessionStorage.clear();
+}
+
+
+// TODO: Potentially personalize Welcome message with first name from input
+// document.getElementById("profileModalTitle").innerHTML = "Welcome to the TVSensei Family " + localStorage.getItem("fname") + "!";
